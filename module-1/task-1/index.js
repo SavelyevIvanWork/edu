d00157d2af06089090ec3541a0fd95bb606ad67b
Savelyev Ivan <savelyev.exceedteam@gmail>
2021-07-03 21:41:18 +0300

    first commit

---
+++ module-1/task-1/index.js
@@ -0,0 +1,8 @@
+const a = 10
+const b = 10434
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+console.log(sum(a, b))
---
+++ module-1/task-2/index.js
@@ -0,0 +1,8 @@
+const a = 10
+const b = 1000
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+sum(a, b)
